df11da580f23928d13369f8059c1993953ae6c0b
David Reiman <david.reiman@gmail.com>
2013-12-10 17:49:36 -0800

    Pivot Index, efficient in space if not thoroughly efficient in time

---
+++ pivot_index.rb
@@ -0,0 +1,13 @@
+def pivot_index(array)
+  total = array.inject(:+)
+  running_count = 0
+  array.each_index do |index|
+  	if running_count + array[index] + running_count == total
+  		return index
+  	else
+  		running_count += array[index]
+  	end
+  end
+end
+
+puts pivot_index([1,3,2,7,5,1]) == 3
