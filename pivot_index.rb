ec0b8481e39bd454c30cdeb7128ea40072a815ad
elreimundo <david.reiman@gmail.com>
2013-12-12 16:10:03 -0800

    Now accounts for case where pivot index is not found, returning -1

--- pivot_index.rb
+++ pivot_index.rb
@@ -2,12 +2,13 @@ def pivot_index(array)
   total = array.inject(:+)
   running_count = 0
   array.each_index do |index|
-  	if running_count + array[index] + running_count == total
-  		return index
-  	else
-  		running_count += array[index]
-  	end
+    if running_count + array[index] + running_count == total
+      return index
+    else
+      running_count += array[index]
+    end
   end
+  -1
 end
 
 puts pivot_index([1,3,2,7,5,1]) == 3
