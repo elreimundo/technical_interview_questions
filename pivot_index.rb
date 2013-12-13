be6f427c4bbcdd1fafc32de444f666e3ec68a81d
David Reiman <david.reiman@gmail.com>
2013-12-12 16:25:30 -0800

    Adds pivot index with hash method along with tests for pivot index = -1

--- pivot_index.rb
+++ pivot_index.rb
@@ -11,4 +11,17 @@ def pivot_index(array)
   -1
 end
 
+def pivot_index_with_hashes_this_time(array)
+  running_counts = Hash.new
+  running_count = 0
+  array.each_index do |index|
+    running_counts[running_count + array[index] + running_count] = index
+    running_count += array[index]
+  end
+  running_counts[running_count] || -1
+end
+
 puts pivot_index([1,3,2,7,5,1]) == 3
+puts pivot_index_with_hashes_this_time([1,3,2,7,5,1]) == 3
+puts pivot_index([1,3,2,7,5]) == -1
+puts pivot_index_with_hashes_this_time([1,3,2,7,5]) == -1
